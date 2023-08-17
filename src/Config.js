0c696a110a21691ade9f4e8669fda9fe2748f11f
Gautam Raj <gautamraj996@gmail.com>
2023-08-17 22:11:46 +0530

    api url updated

--- src/Config.js
+++ src/Config.js
@@ -1,3 +1,3 @@
 // const API_URL = 'https://localhost:5000/api';
-const API_URL = 'https://clever-bass-cuff-links.cyclic.cloud/api';
+const API_URL = 'https://rohan-raj-blog-backend.onrender.com';
 export default API_URL;
