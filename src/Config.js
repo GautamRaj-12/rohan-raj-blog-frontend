375aeb668eb336c14b731a9fce4b76de50cf2f52
Gautam Raj <gautamraj996@gmail.com>
2023-08-17 22:43:34 +0530

    api url modified

--- src/Config.js
+++ src/Config.js
@@ -1,3 +1,3 @@
 // const API_URL = 'https://localhost:5000/api';
-const API_URL = 'https://rohan-raj-blog-backend.onrender.com';
+const API_URL = 'https://rohan-raj-blog-backend.onrender.com/api';
 export default API_URL;
