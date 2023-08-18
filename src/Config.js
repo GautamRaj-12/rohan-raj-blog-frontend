07d76981377b7427bd65b6ba8c84af41365017f8
Gautam Raj <gautamraj996@gmail.com>
2023-08-18 06:04:51 +0530

    cyclic api added

--- src/Config.js
+++ src/Config.js
@@ -1,3 +1,4 @@
 // const API_URL = 'https://localhost:5000/api';
-const API_URL = 'https://rohan-raj-blog-backend.onrender.com/api';
+// const API_URL = 'https://rohan-raj-blog-backend.onrender.com/api';
+const API_URL = 'https://rohan-blogs-backend.cyclic.cloud/api';
 export default API_URL;
