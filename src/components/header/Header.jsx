e7133893bf46c1ba66dc7a7756eda3da2d1ec40f
Gautam Raj <gautamraj996@gmail.com>
2023-08-19 17:00:55 +0530

    header design modified

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,31 @@
-import './header.css'
+import './header.css';
 
 function Header() {
   return (
-    <div className="header">
-      <h2 className="mb-4">Welcome to <div>Rohan's Desk</div></h2>
+    <div className='header'>
+      <h2 className='mb-4 animate__animated animate__fadeInDown'>
+        Rohan's Desk
+      </h2>
       <h3>
-        My point of view on different areas specially
-        <div>law,cricket and administration.</div>
+        <div className='animate__animated animate__fadeInUp'>
+          <span className='animate__animated animate__fadeInUp'>Where</span>{' '}
+          <span className='header-span animate__animated animate__fadeInUp'>
+            Law
+          </span>{' '}
+          <span className='header-span animate__animated animate__fadeInUp'>
+            Cricket
+          </span>{' '}
+          <span>and </span>
+          <span className='header-span animate__animated animate__fadeInUp'>
+            Administration
+          </span>
+        </div>
+        <div className='animate__animated animate__fadeInUp'>
+          are played on One Turf{' '}
+        </div>
       </h3>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
