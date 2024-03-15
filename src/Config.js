7b728702f4bd9c8a57b53172788573bd69b6aabe
Gautam Raj <gautamraj996@gmail.com>
2024-03-15 18:13:12 +0530

    Update Config.js

--- src/Config.js
+++ src/Config.js
@@ -1,4 +1,5 @@
 // const API_URL = 'https://localhost:5000/api';
-const API_URL = 'https://rohan-raj-blog-backend.onrender.com/api';
+// const API_URL = 'https://rohan-raj-blog-backend.onrender.com/api';
 // const API_URL = 'https://rohan-blogs-backend.cyclic.cloud/api';
+const API_URL = 'https://urchin-app-rhrb5.ondigitalocean.app/';
 export default API_URL;
